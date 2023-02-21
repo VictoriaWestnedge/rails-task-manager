21aeb3cca02f7c631bba6f81c01a99aac232d8dc
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-02-21 15:32:02 -0300

    Rails-Task-Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  delete "tasks/:id", to: "tasks#destroy"
 
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks", to: "tasks#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
